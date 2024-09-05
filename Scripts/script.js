7d33f637a9ba683485e5584378d055e239561788
ANANDHU P DIVAKARAN <anandhupdivakaran61@gmail.com>
2024-09-05 09:47:54 +0530

    Add files via upload

---
+++ Scripts/script.js
@@ -0,0 +1,9 @@
+const backTop = document.getElementById('backTop');
+
+function backNav() {
+    backTop.style.display = "inline";
+  }
+  
+  function normalPosition() {
+    backTop.style.display = "none";
+  }
